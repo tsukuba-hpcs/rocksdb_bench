50092d669ddcb437e598a8ff2d250f3518ba4c38
onokatio <onokatio@gmail.com>
2023-12-05 15:02:11 +0900

    add chfs code

---
+++ vendor/fs_posix.c
@@ -0,0 +1,608 @@
+#include "config.h"
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <margo.h>
+#ifdef USE_ABT_IO
+#include <abt-io.h>
+#endif
+
+#include "ring_types.h"
+#include "ring_list.h"
+#include "kv_types.h"
+#include "kv.h"
+#include "kv_err.h"
+#include "fs_types.h"
+#include "fs.h"
+#include "file.h"
+#include "log.h"
+
+#ifndef USE_XATTR
+struct metadata {
+	size_t chunk_size;
+};
+
+static int msize = sizeof(struct metadata);
+#else
+static int msize = 0;
+#endif
+
+#ifdef USE_ABT_IO
+static abt_io_instance_id abtio;
+static __thread int __r;
+
+#define open(path, flags, mode) \
+	((__r = abt_io_open(abtio, path, flags, mode)) < 0 ? \
+	 (errno = -__r), -1 : __r)
+#define close(fd) abt_io_close(abtio, fd)
+#define write(fd, buf, count) \
+	((__r = abt_io_write(abtio, fd, buf, count)) < 0 ? \
+	 (errno = -__r), -1 : __r)
+#define read(fd, buf, count) \
+	((__r = abt_io_read(abtio, fd, buf, count)) < 0 ? \
+	 (errno = -__r), -1 : __r)
+#define pwrite(fd, buf, count, off) \
+	((__r = abt_io_pwrite(abtio, fd, buf, count, off)) < 0 ? \
+	 (errno = -__r), -1 : __r)
+#define pread(fd, buf, count, off) \
+	((__r = abt_io_pread(abtio, fd, buf, count, off)) < 0 ? \
+	 (errno = -__r), -1 : __r)
+#ifdef HAVE_ABT_IO_TRUNCATE
+#define truncate(path, len) \
+	((__r = abt_io_truncate(abtio, path, len)) < 0 ? \
+	 (errno = -__r), -1 : __r)
+#endif
+#define unlink(path) \
+	((__r = abt_io_unlink(abtio, path)) < 0 ? (errno = -__r), -1 : __r)
+#endif
+
+static int
+fs_err(int err, const char *diag)
+{
+	if (err >= 0)
+		return (KV_SUCCESS);
+
+	switch (-err) {
+	case EEXIST:
+		return (KV_ERR_EXIST);
+	case ENOENT:
+		return (KV_ERR_NO_ENTRY);
+	case ENOMEM:
+		return (KV_ERR_NO_MEMORY);
+	case ENOTSUP:
+		return (KV_ERR_NOT_SUPPORTED);
+	default:
+		log_notice("fs_err (%s): %s", diag, strerror(-err));
+		return (KV_ERR_UNKNOWN);
+	}
+}
+
+void
+fs_inode_init(char *dir, int niothreads)
+{
+	int r;
+
+	r = chdir(dir);
+	if (r == -1 && errno == ENOENT) {
+		r = mkdir_p(dir, 0755);
+		if (r == 0)
+			r = chdir(dir);
+	}
+	if (r == -1)
+		log_fatal("%s: %s", dir, strerror(errno));
+
+#ifdef USE_ABT_IO
+	abtio = abt_io_init(niothreads);
+	if (abtio == ABT_IO_INSTANCE_NULL)
+		log_fatal("abt_io_init failed, abort");
+#endif
+	log_info("fs_inode_init: path %s", dir);
+}
+
+/* key is modified */
+static char *
+key_to_path(char *key, size_t key_size)
+{
+	size_t klen = strlen(key);
+	static const char diag[] = "key_to_path";
+
+	log_debug("%s: key %s", diag, key);
+	if (klen + 1 < key_size)
+		key[klen] = ':';
+	while (*key == '/')
+		++key;
+	if (*key == '\0')
+		key = ".";
+	log_debug("%s: path %s", diag, key);
+	return (key);
+}
+
+static char *
+fs_dirname(const char *path)
+{
+	size_t p = strlen(path) - 1;
+	char *r;
+
+	while (p > 0 && path[p] != '/')
+		--p;
+	if (p == 0)
+		return (NULL);
+
+	r = malloc(p + 1);
+	if (r == NULL) {
+		log_error("fs_dirname: no memory");
+		return (NULL);
+	}
+	strncpy(r, path, p);
+	r[p] = '\0';
+	log_debug("fs_dirname: path %s dirname %s", path, r);
+	return (r);
+}
+
+#define FS_XATTR_CHUNK_SIZE "user.chunk_size"
+
+static int
+set_chunk_size(const char *path, size_t size)
+{
+	int r;
+	static const char diag[] = "set_chunk_size";
+#ifndef USE_XATTR
+	int fd;
+	struct metadata mdata;
+#endif
+
+#ifdef USE_XATTR
+	r = setxattr(path, FS_XATTR_CHUNK_SIZE, &size, sizeof(size), 0);
+	if (r == -1) {
+		r = -errno;
+		log_error("%s: %s: %s", diag, path, strerror(errno));
+	}
+#else
+	fd = open(path, O_WRONLY, 0);
+	if (fd == -1) {
+		r = -errno;
+		log_error("%s: %s: %s", diag, path, strerror(errno));
+	} else {
+		mdata.chunk_size = size;
+		r = write(fd, &mdata, msize);
+		if (r == -1) {
+			r = -errno;
+			log_error("%s (write): %s", diag, strerror(errno));
+		} else if (r != msize) {
+			log_error("%s (write): %d of %d bytes written", diag,
+				r, msize);
+			r = -ENOSPC;
+		}
+		close(fd);
+	}
+#endif
+	return (r);
+}
+
+static int
+get_chunk_size(const char *path, size_t *size)
+{
+	int r;
+	static const char diag[] = "get_chunk_size";
+#ifndef USE_XATTR
+	int fd;
+	struct metadata mdata;
+#endif
+
+#ifdef USE_XATTR
+	r = getxattr(path, FS_XATTR_CHUNK_SIZE, size, sizeof(*size));
+	if (r == -1) {
+		r = -errno;
+		log_info("%s: %s: %s", diag, path, strerror(errno));
+	}
+#else
+	fd = open(path, O_RDONLY, 0);
+	if (fd == -1) {
+		r = -errno;
+		log_info("%s: %s: %s", diag, path, strerror(errno));
+	} else {
+		r = read(fd, &mdata, msize);
+		if (r == -1) {
+			r = -errno;
+			log_error("%s (read): %s", diag, strerror(errno));
+		} else if (r != msize) {
+			log_error("%s (read): %d of %d bytes read", diag, r,
+				msize);
+			r = -EIO;
+		} else
+			*size = mdata.chunk_size;
+		close(fd);
+	}
+#endif
+	return (r);
+}
+
+static int
+fs_open(const char *path, int flags, mode_t mode, size_t *chunk_size)
+{
+	int fd, r = 0;
+	char *d;
+
+	if ((flags & O_ACCMODE) == O_RDONLY) {
+		r = get_chunk_size(path, chunk_size);
+		if (r < 0)
+			return (r);
+	}
+	fd = open(path, flags, mode);
+	if (fd == -1 && ((flags & O_ACCMODE) != O_RDONLY)) {
+		d = fs_dirname(path);
+		if (d != NULL) {
+			/* mkdir_p() may fail due to race condition */
+			mkdir_p(d, 0755);
+			free(d);
+		}
+		flags |= O_CREAT;
+		fd = open(path, flags, mode);
+	}
+	if (fd == -1)
+		return (-errno);
+	if (flags & O_CREAT) {
+		r = set_chunk_size(path, *chunk_size);
+		if (r < 0)
+			close(fd);
+	}
+	return (r < 0 ? r : fd);
+}
+
+int
+fs_inode_create(char *key, size_t key_size, uint32_t uid, uint32_t gid,
+	mode_t mode, size_t chunk_size, const void *buf, size_t size)
+{
+	char *p = key_to_path(key, key_size), *d;
+	int r, fd;
+	static const char diag[] = "fs_inode_create";
+
+	log_debug("%s: %s mode %o chunk_size %ld", diag, p, mode, chunk_size);
+	if (S_ISREG(mode)) {
+		fd = r = fs_open(p, O_CREAT|O_WRONLY|O_TRUNC, mode,
+			&chunk_size);
+		if (fd >= 0) {
+			if (buf && size > 0) {
+				if (size > chunk_size)
+					size = chunk_size;
+				r = pwrite(fd, buf, size, msize);
+				if (r == -1)
+					r = -errno;
+			}
+			close(fd);
+		}
+	} else if (S_ISDIR(mode)) {
+		r = mkdir_p(p, mode);
+		if (r == -1)
+			r = -errno;
+	} else if (S_ISLNK(mode)) {
+		r = symlink(buf, p);
+		if (r == -1) {
+			d = fs_dirname(p);
+			if (d != NULL) {
+				/* mkdir_p() may fail due to race condition */
+				mkdir_p(d, 0755);
+				free(d);
+			}
+			r = symlink(buf, p);
+		}
+		if (r == -1)
+			r = -errno;
+	} else
+		r = -ENOTSUP;
+	if (r < 0)
+		log_error("%s: %s (%o): %s", diag, p, mode, strerror(-r));
+
+	return (fs_err(r, diag));
+}
+
+int
+fs_inode_create_stat(char *key, size_t key_size, struct fs_stat *st,
+	const void *buf, size_t size)
+{
+	char *p = key_to_path(key, key_size);
+	struct timespec times[2];
+	int r, fd;
+	static const char diag[] = "fs_inode_create_stat";
+
+	log_debug("%s: %s mode %o chunk_size %ld", diag, p, st->mode,
+		st->chunk_size);
+	if (S_ISREG(st->mode)) {
+		fd = r = fs_open(p, O_CREAT|O_WRONLY|O_TRUNC, st->mode,
+			&st->chunk_size);
+		if (fd >= 0) {
+			if (buf && size > 0) {
+				if (size > st->chunk_size + msize)
+					size = st->chunk_size + msize;
+				r = pwrite(fd, buf, size, 0);
+				if (r == -1)
+					r = -errno;
+			}
+			close(fd);
+		}
+	} else
+		r = fs_inode_create(key, key_size, st->uid, st->gid, st->mode,
+			st->chunk_size, buf, size);
+	if (r == KV_SUCCESS) {
+		times[0] = times[1] = st->mtime;
+		utimensat(AT_FDCWD, p, times, AT_SYMLINK_NOFOLLOW);
+	}
+	return (r);
+}
+
+int
+fs_inode_stat(char *key, size_t key_size, struct fs_stat *st)
+{
+	char *p = key_to_path(key, key_size);
+	struct stat sb;
+	int r;
+	static const char diag[] = "fs_inode_stat";
+
+	log_debug("%s: %s", diag, p);
+	r = lstat(p, &sb);
+	if (r == -1) {
+		r = -errno;
+		goto err;
+	}
+	if (S_ISREG(sb.st_mode)) {
+		r = get_chunk_size(p, &st->chunk_size);
+		if (r < 0)
+			goto err;
+	} else
+		st->chunk_size = 0;
+
+	st->mode = sb.st_mode;
+	st->uid = sb.st_uid;
+	st->gid = sb.st_gid;
+	st->size = sb.st_size;
+	if (S_ISREG(sb.st_mode))
+		st->size -= msize;
+	st->mtime = sb.st_mtim;
+	st->ctime = sb.st_ctim;
+err:
+	log_debug("%s: %d", diag, r);
+	return (fs_err(r, diag));
+}
+
+int
+fs_inode_write(char *key, size_t key_size, const void *buf, size_t *size,
+	off_t offset, mode_t mode, size_t chunk_size)
+{
+	char *p = key_to_path(key, key_size);
+	size_t ss;
+	int fd, r = 0;
+	static const char diag[] = "fs_inode_write";
+
+	log_debug("%s: %s size %ld offset %ld", diag, p, *size, offset);
+	ss = *size;
+	if (ss + offset > chunk_size) {
+		if (offset >= chunk_size) {
+			*size = 0;
+			goto err;
+		}
+		ss = chunk_size - offset;
+	}
+	fd = r = fs_open(p, O_WRONLY, mode, &chunk_size);
+	if (fd >= 0) {
+		r = pwrite(fd, buf, ss, offset + msize);
+		if (r == -1)
+			r = -errno;
+		close(fd);
+	}
+	if (r >= 0)
+		*size = r;
+err:
+	if (r < 0)
+		log_error("%s: %s: %s", diag, p, strerror(-r));
+	else
+		log_debug("%s: %s: ret %d", diag, p, r);
+	return (fs_err(r, diag));
+}
+
+int
+fs_inode_read(char *key, size_t key_size, void *buf, size_t *size,
+	off_t offset)
+{
+	char *p = key_to_path(key, key_size);
+	struct stat sb;
+	size_t ss, chunk_size;
+	int fd, r;
+	static const char diag[] = "fs_inode_read";
+
+	log_debug("%s: %s size %ld offset %ld", diag, p, *size, offset);
+	if (lstat(p, &sb) == 0 && S_ISLNK(sb.st_mode)) {
+		r = readlink(p, buf, *size);
+		if (r == -1)
+			r = -errno;
+		else
+			*size = r;
+		goto done;
+	}
+	fd = r = fs_open(p, O_RDONLY, 0644, &chunk_size);
+	if (r < 0)
+		goto done;
+	log_debug("%s: chunk_size %ld", diag, chunk_size);
+
+	ss = *size;
+	if (ss + offset > chunk_size) {
+		if (offset >= chunk_size)
+			ss = 0;
+		else
+			ss = chunk_size - offset;
+	}
+	if (ss == 0)
+		r = 0;
+	else {
+		r = pread(fd, buf, ss, offset + msize);
+		if (r == -1)
+			r = -errno;
+	}
+	close(fd);
+	if (r < 0)
+		goto done;
+	*size = r;
+done:
+	log_debug("%s: ret %d", diag, r);
+	return (fs_err(r, diag));
+}
+
+static char *
+make_path(const char *dir, const char *entry)
+{
+	int dir_len, entry_len, slash = 1;
+	char *p;
+
+	if (dir == NULL || entry == NULL)
+		return (NULL);
+
+	dir_len = strlen(dir);
+	entry_len = strlen(entry);
+
+	if (dir_len > 0 && dir[dir_len - 1] == '/')
+		slash = 0;
+	p = malloc(dir_len + slash + entry_len + 1);
+	if (p == NULL)
+		return (NULL);
+	strcpy(p, dir);
+	if (slash)
+		strcat(p, "/");
+	strcat(p, entry);
+
+	return (p);
+}
+
+static int
+rmdir_r(const char *dir)
+{
+	DIR *d;
+	struct dirent *dent;
+	char *p;
+	int r, save_errno;
+
+	r = rmdir(dir);
+	if (r == 0 || (errno != ENOTEMPTY && errno != EEXIST))
+		return (r);
+
+	d = opendir(dir);
+	if (d == NULL)
+		return (-1);
+
+	while ((dent = readdir(d)) != NULL) {
+		if (dent->d_name[0] == '.' && (dent->d_name[1] == '\0' ||
+		    (dent->d_name[1] == '.' && dent->d_name[2] == '\0')))
+			continue;
+
+		p = make_path(dir, dent->d_name);
+		if (p == NULL) {
+			r = -1;
+			errno = ENOMEM;
+			break;
+		}
+		r = rmdir_r(p);
+		free(p);
+		if (r == -1)
+			break;
+	}
+	save_errno = errno;
+	closedir(d);
+	errno = save_errno;
+	if (r == 0)
+		r = rmdir(dir);
+	return (r);
+}
+
+int
+fs_inode_truncate(char *key, size_t key_size, off_t len)
+{
+	char *p = key_to_path(key, key_size);
+	int r;
+	static const char diag[] = "fs_inode_truncate";
+
+	log_debug("%s: %s len %ld", diag, p, len);
+	r = truncate(p, len + msize);
+	if (r == -1)
+		r = -errno;
+	return (fs_err(r, diag));
+}
+
+int
+fs_inode_remove(char *key, size_t key_size)
+{
+	char *p = key_to_path(key, key_size);
+	struct stat sb;
+	int r;
+	static const char diag[] = "fs_inode_remove";
+
+	log_debug("%s: %s", diag, p);
+	if (lstat(p, &sb) == -1)
+		return (fs_err(-errno, diag));
+
+	if (S_ISDIR(sb.st_mode))
+		r = rmdir_r(p);
+	else
+		r = unlink(p);
+	if (r == -1)
+		r = -errno;
+	return (fs_err(r, diag));
+}
+
+int
+fs_inode_readdir(char *path, void (*cb)(struct dirent *, struct stat *, void *),
+	void *arg)
+{
+	char *p = key_to_path(path, strlen(path) + 1);
+	struct dirent *dent;
+	DIR *dp;
+	struct stat sb;
+	int r;
+	static const char diag[] = "fs_inode_readdir";
+
+	log_debug("%s: %s", diag, p);
+	dp = opendir(p);
+	if (dp != NULL) {
+		r = 0;
+		while ((dent = readdir(dp)) != NULL) {
+			if (strchr(dent->d_name, ':'))
+				continue;
+			if (fstatat(dirfd(dp), dent->d_name, &sb,
+				AT_SYMLINK_NOFOLLOW))
+				continue;
+			if (S_ISREG(sb.st_mode))
+				sb.st_size -= msize;
+			cb(dent, &sb, arg);
+		}
+		closedir(dp);
+	} else
+		r = -errno;
+
+	return (fs_err(r, diag));
+}
+
+int
+fs_inode_unlink_chunk_all(char *path, int i)
+{
+	char p[PATH_MAX];
+	int len, klen;
+
+	if (path == NULL)
+		return (0);
+	len = strlen(path);
+	strcpy(p, path);
+	for (;; ++i) {
+		sprintf(p + len + 1, "%d", i);
+		klen = len + 1 + strlen(p + len + 1) + 1;
+		if (!ring_list_is_in_charge(p, klen))
+			continue;
+		p[len] = ':';
+		if (unlink(p))
+			break;
+		p[len] = '\0';
+	}
+	return (0);
+}
---
+++ vendor/fs_server_posix.c
@@ -0,0 +1,155 @@
+#include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <margo.h>
+#include "config.h"
+#include "ring.h"
+#include "ring_types.h"
+#include "ring_rpc.h"
+#include "ring_list.h"
+#include "kv_types.h"
+#include "kv_err.h"
+#include "kv.h"
+#include "fs_types.h"
+#include "fs_rpc.h"
+#include "fs.h"
+#include "log.h"
+
+static char *self;
+
+DECLARE_MARGO_RPC_HANDLER(inode_readdir)
+
+void
+fs_server_init_more(margo_instance_id mid, char *db_dir, size_t db_size,
+	int niothreads)
+{
+	hg_id_t read_rdma_rpc = -1, readdir_rpc;
+
+#ifdef USE_ZERO_COPY_READ_RDMA
+#error posix backend does not support --enable-zero-copy-read-rdma
+#endif
+	readdir_rpc = MARGO_REGISTER(mid, "inode_readdir", hg_string_t,
+		fs_readdir_out_t, inode_readdir);
+
+	fs_client_init_more_internal(read_rdma_rpc, readdir_rpc);
+	fs_inode_init(db_dir, niothreads);
+
+	self = ring_get_self();
+}
+
+void
+fs_server_term_more()
+{}
+
+struct fs_readdir_arg {
+	char path[PATH_MAX];
+	int n, size, pathlen;
+	fs_file_info_t *fi;
+};
+
+static void
+free_fs_readdir_arg(struct fs_readdir_arg *a)
+{
+	int i;
+
+	for (i = 0; i < a->n; ++i)
+		free(a->fi[i].name);
+	free(a->fi);
+}
+
+static void
+fs_add_entry(struct dirent *dent, struct stat *st, void *arg)
+{
+	struct fs_readdir_arg *a = arg;
+	fs_file_info_t *tfi;
+	int namelen = strlen(dent->d_name);
+
+	log_debug("add_entry: %s", dent->d_name);
+	if (a->pathlen + namelen + 1 > PATH_MAX) {
+		log_error("fs_add_entry: too long: %s%s (%d)", a->path,
+			dent->d_name, a->pathlen + namelen + 1);
+		return;
+	}
+	strcpy(a->path + a->pathlen, dent->d_name);
+	if (a->n >= a->size) {
+		tfi = realloc(a->fi, sizeof(a->fi[0]) * a->size * 2);
+		if (tfi == NULL) {
+			log_error("fs_add_entry: no memory");
+			return;
+		}
+		a->fi = tfi;
+		a->size *= 2;
+	}
+	a->fi[a->n].name = strdup(dent->d_name);
+	if (a->fi[a->n].name == NULL) {
+		log_error("fs_add_entry: no memory");
+		return;
+	}
+	memset(&a->fi[a->n].sb, 0, sizeof(a->fi[a->n].sb));
+	a->fi[a->n].sb.mode = st->st_mode;
+	if (!ring_list_is_in_charge(a->path, a->pathlen + namelen + 1))
+		a->fi[a->n].sb.mode |= CHFS_S_IFREP;
+	a->fi[a->n].sb.size = st->st_size;
+	a->fi[a->n].sb.uid = st->st_uid;
+	a->fi[a->n].sb.gid = st->st_gid;
+	a->fi[a->n].sb.mtime = st->st_mtim;
+	a->fi[a->n].sb.ctime = st->st_ctim;
+	++a->n;
+}
+
+static void
+inode_readdir(hg_handle_t h)
+{
+	hg_string_t path;
+	struct fs_readdir_arg a;
+	fs_readdir_out_t out;
+	hg_return_t ret;
+	static const char diag[] = "inode_readdir RPC";
+
+	ret = margo_get_input(h, &path);
+	if (ret != HG_SUCCESS) {
+		log_error("%s (get_input): %s", diag, HG_Error_to_string(ret));
+		goto destroy;
+	}
+	log_debug("%s: path=%s", diag, path);
+
+	memset(&out, 0, sizeof(out));
+	a.n = 0;
+	a.size = 1000;
+	a.fi = malloc(sizeof(a.fi[0]) * a.size);
+	if (a.fi == NULL) {
+		log_error("%s: no memory", diag);
+		out.err = KV_ERR_NO_MEMORY;
+		goto free_input;
+	}
+	a.pathlen = strlen(path);
+	if (a.pathlen > PATH_MAX - 2) {
+		log_error("%s: too long path: %s (%d)", diag, path, a.pathlen);
+		out.err = KV_ERR_TOO_LONG;
+		goto free_input;
+	}
+	strcpy(a.path, path);
+	if (a.pathlen > 0 && a.path[a.pathlen - 1] != '/') {
+		a.path[a.pathlen++] = '/';
+		a.path[a.pathlen] = '\0';
+	}
+	out.err = fs_inode_readdir(path, fs_add_entry, &a);
+	if (out.err == KV_SUCCESS) {
+		out.n = a.n;
+		out.fi = a.fi;
+	}
+free_input:
+	ret = margo_free_input(h, &path);
+	if (ret != HG_SUCCESS)
+		log_error("%s (free_input): %s", diag, HG_Error_to_string(ret));
+
+	ret = margo_respond(h, &out);
+	if (ret != HG_SUCCESS)
+		log_error("%s (respond): %s", diag, HG_Error_to_string(ret));
+	free_fs_readdir_arg(&a);
+destroy:
+	ret = margo_destroy(h);
+	if (ret != HG_SUCCESS)
+		log_error("%s (destroy): %s", diag, HG_Error_to_string(ret));
+}
+DEFINE_MARGO_RPC_HANDLER(inode_readdir)
